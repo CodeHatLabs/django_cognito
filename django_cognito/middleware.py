936180871ed4df7d019910f1c9d1b91a7b97f4e0
Jeff Jarvis <jjarvis@codehatlabs.com>
2018-04-28 23:51:18 -0700

    convert to 1.10-style middleware

--- django_cognito/middleware.py
+++ django_cognito/middleware.py
@@ -1,18 +1,14 @@
-from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
 
 from django_cognito.auth import *
 
 
-class AuthenticationMiddleware(MiddlewareMixin):
+class AuthenticationMiddleware(object):
 
-    def process_request(self, request):
-        assert hasattr(request, 'session'), (
-            "The django_cognito authentication middleware requires session "
-            "middleware to be installed. Edit your MIDDLEWARE setting to "
-            "insert 'django.contrib.sessions.middleware.SessionMiddleware' "
-            "before 'django_cognito.middleware.AuthenticationMiddleware'."
-            )
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         session = request.session
         request.cognito_user = SimpleLazyObject(
             lambda: CognitoUser(
@@ -22,5 +18,6 @@ class AuthenticationMiddleware(MiddlewareMixin):
                 uuid = session[USER_UUID]
                 ) if USER_UUID in session else CognitoGuest()
             )
+        return self.get_response(request)
 
 
